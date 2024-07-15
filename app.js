4cd3c9e18f13a8f5a2bd00721d17c1283bc1c3ea
SkSelim-Git <skselim0777@gmail.com>
2024-07-15 13:34:20 +0530

    add

--- app.js
+++ app.js
@@ -31,10 +31,10 @@ menuItems.forEach(item => {
         changeActiveItem();
         item.classList.add("active");
         if(item.id != "notifications"){
-            document.querySelector("notifications-popup").style.display = "none";
+            document.querySelector(".notifications-popup").style.display = "none";
         }else{
-            document.querySelector("notifications-popup").style.display = "block";
-            document.querySelector("#notifications.notification-count").style.display = "none";
+            document.querySelector(".notifications-popup").style.display = "block";
+            document.querySelector("#notifications .notification-count").style.display = "none";
         }
     })
 })
